fe4033b8acd3ee6a96085cb0ccd5e4cac7ece164
JDamianHdezMartin <damian.hdez.martin@gmail.com>
2022-03-30 16:11:47 +0100

    Se termina la tarea asignada para el 30 de marzo

---
+++ ListaTexto1/src/lista/controller/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author daw
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private TextField txtField;
+    @FXML
+    private Button buttonGuardar;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void guardar(ActionEvent event) {
+        listView.getItems().add(txtField.getText());
+    }
+    
+}
